516fe895e16181d7ad1d79a493fbf44e5354e382
tljk <tanglinchuanz@gmail.com>
2024-11-11 15:57:23 -0500

    fix missing sudo

--- setup_ubuntu22.sh
+++ setup_ubuntu22.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt -y install make git curl wget traceroute net-tools libpcap-dev build-essential openssl
-snap install --classic go
-snap install golangci-lint --classic
+sudo snap install --classic go
+sudo snap install golangci-lint --classic
 # chrome setup
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
